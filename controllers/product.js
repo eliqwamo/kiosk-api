c83f7c700791c8f99d6957610475f724fc0d6c5a
eliqwamo <eli@qwamo.com>
2022-03-30 14:02:27 +0300

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -8,7 +8,32 @@ const Store = require('../models/store');
 const Category = require('../models/category');
 const Product = require('../models/product');
 
-router.post('/addCategory', isAuth, async(request,response) => {});
+router.post('/addCategory', isAuth, async(request,response) => {
+    const accountId = request.account._id;
+    const store = await Store.findOne({associateId: accountId});
+    const categoryId = mongoose.Types.ObjectId();
+    const { categoryName, categoryImage, priority } = request.body;
+    const _category = new Category({
+        _id: categoryId,
+        storeId: store._id,
+        categoryName: categoryName,
+        categoryImage: categoryImage,
+        priority: priority
+    });
+    _category.save()
+    .then(category_created => {
+        return response.status(200).json({
+            status: true,
+            message: category_created
+        });
+    })
+    .catch(err => {
+        return response.status(500).json({
+            status: false,
+            message: err
+        });
+    })
+});
 router.put('/updateCategory', isAuth, async(request,response) => { });
 router.delete('/deleteCategory', isAuth, async(request,response) => { });
 router.post('/addProduct', isAuth, async(request,response) => { });
